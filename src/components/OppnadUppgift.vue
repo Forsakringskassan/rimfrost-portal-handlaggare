420caaf726620971ee3de9362d290243dbc35012
Lisa Wedin <lisa.wedin@ductus.se>
2026-01-21 08:35:39 +0100

    fix: removes unused function

--- src/components/OppnadUppgift.vue
+++ src/components/OppnadUppgift.vue
@@ -10,15 +10,6 @@ const kundbehovsflodeId = computed(() => route.params.id as string | null);
 const regeltyp = computed(() => "rtf-manuell"); // Hardcoded for example
 const componentKey = ref(0);
 
-// const AsyncComponent = defineAsyncComponent(() => {
-//   const remoteUrl = getRegelApiUrl(
-//     "/rtf-manuell/042b3193-ceb6-4edb-95d5-e35675bcdbdc",
-//   );
-//   return import(/* @vite-ignore */ `${remoteUrl}`).then(
-//     (module) => module.default,
-//   );
-// });
-
 watch(
   () => route.params.id,
   () => {
