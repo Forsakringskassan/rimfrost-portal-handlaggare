d991a36652dce8348b5ebc40b46b43fa8cae4497
julolsso <julia.olsson@ductus.se>
2026-05-08 14:10:57 +0200

    feat: added button ladda template MFE

--- src/App.vue
+++ src/App.vue
@@ -110,10 +110,12 @@ async function handleGetNextUppgift() {
             </div>
           </div>
           <div class="nav-footer">
-            <FButton variant="secondary" @click="router.push('/dev/template')"
+            <FButton @click="handleGetNextUppgift">Hämta ny uppgift</FButton>
+            <FButton
+              variant="secondary"
+              @click="router.push({ name: 'dev-template' })"
               >Ladda template MFE</FButton
             >
-            <FButton @click="handleGetNextUppgift">Hämta ny uppgift</FButton>
             <p v-if="getNextUppgiftFel" class="error-message">
               {{ getNextUppgiftFel }}
             </p>
